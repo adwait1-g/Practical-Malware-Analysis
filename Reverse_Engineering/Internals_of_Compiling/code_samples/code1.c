4b0d51f00fdc21d6665bcc6689e0bf6368f520fe
adwait1-g <adwait.gautham@gmail.com>
2018-05-09 19:29:15 +0530

    Added complete post - Internals of compiling

---
+++ Reverse_Engineering/Internals_of_Compiling/code_samples/code1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int a = 10;		//Global variable 'a' initialized to 10.
+int b;			//Global variable uninitialized.
+
+int main() {
+	int c = 123;
+	char d = 'x';
+
+	printf("Hello world!!\n");
+
+	return 0;
+}
